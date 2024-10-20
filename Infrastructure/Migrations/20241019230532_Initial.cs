daba8156427309e0e25115d0fe203f88be1c89fd
Abdulfattah Hessein <abdulfattah.hessein@gmail.com>
2024-10-20 19:14:32 +0300

    add .csharpierignore file and add Migration folder to it to prevent format its files

--- Infrastructure/Migrations/20241019230532_Initial.cs
+++ Infrastructure/Migrations/20241019230532_Initial.cs
@@ -14,9 +14,7 @@ namespace Infrastructure.Migrations
                 name: "Products",
                 columns: table => new
                 {
-                    Id = table
-                        .Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<int>(type: "int", nullable: false).Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
